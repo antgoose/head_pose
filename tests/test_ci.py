e575778df2ce4f2709806f28ffe97069046f2d4a
antgoosev <antongoosev@gmail.com>
2022-06-09 22:20:23 +0300

    Add workflow

---
+++ tests/test_ci.py
@@ -0,0 +1,14 @@
+import pytest
+from demo import head_pose
+import cv2
+
+
+def test_regression():
+    status = 0
+    image1 = head_pose()
+    image2 = cv2.imread("reference", 0)
+    if image1.shape == image2.shape and not (np.bitwise_xor(image1, image2).any()):
+        status = 1
+    else:
+        status = 0
+    assert status == 1
