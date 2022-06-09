271fd6bc5ef0cf6e02e11dbb40480037aeef66e4
antgoosev <antongoosev@gmail.com>
2022-06-10 02:18:13 +0300

    Add no-error check + images

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -4,14 +4,21 @@ import cv2
 import pkg_resources
 import numpy as np
 
+path_to_file = pkg_resources.resource_filename("tests", "reference.jpeg")
+path_to_big = pkg_resources.resource_filename("tests", "big_image.jpeg")
+path_to_small = pkg_resources.resource_filename("tests", "small_image.jpeg")
+
 
 def test_regression():
     status = 0
     image1 = head_pose()
-    path_to_file = pkg_resources.resource_filename("tests", "reference.jpeg")
     image2 = cv2.imread(path_to_file, 0)
     if image1.shape == image2.shape and not (np.bitwise_xor(image1, image2).any()):
         status = 1
     else:
         status = 0
     assert status == 0 # Do it because can't download file for regression due to opencv permission error
+
+def test_no_error():
+    head_pose(path_to_big)
+    head_pose(path_to_small)
