5266d56b666d35ccd637a4ffc2b899bc8d1b79cb
antgoosev <antongoosev@gmail.com>
2022-06-10 02:00:47 +0300

    Add pkg_resources paths to test_ci.py

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -1,12 +1,14 @@
 import pytest
 from src.demo import head_pose
 import cv2
+import pkg_resources
 
 
 def test_regression():
     status = 0
     image1 = head_pose()
-    image2 = cv2.imread("reference.jpeg", 0)
+    path_to_file = pkg_resources.resource_filename("tests", "reference.jpeg")
+    image2 = cv2.imread(path_to_file, 0)
     if image1.shape == image2.shape and not (np.bitwise_xor(image1, image2).any()):
         status = 1
     else:
